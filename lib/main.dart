368a10ff480bbefe66b5b3511355bcba08423384
Ssebbaale Paul <paulsseb@hotmail.com>
2022-09-10 02:45:44 +0300

    added categories of expences

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:my_expenses/category_model.dart';
 
 void main() {
   runApp(MyApp());
@@ -23,11 +24,77 @@ class _HomePageState extends State<HomePage>
     with SingleTickerProviderStateMixin {
   late TabController _tabController;
   List<String> _tabs = ['Home', 'Category', 'Report'];
+  List<CategoryModel> _IsCategories = List<CategoryModel>();
 
   @override
   void initState() {
     super.initState();
     _tabController = new TabController(vsync: this, length: _tabs.length);
+
+    _initCategories();
+  }
+
+  _initCategories() {
+    var cat1 = CategoryModel().rebuild((b) => b
+      ..id = 0
+      ..title = "Home Utils"
+      ..desc = "Home Utility related expenses"
+      ..iconCodePoint = Icons.home.codePoint);
+
+    _IsCategories.add(cat1);
+
+    var cat2 = CategoryModel().rebuild((b) => b
+      ..id = 0
+      ..title = "Grocery"
+      ..desc = "Grocery related expenses"
+      ..iconCodePoint = Icons.local_grocery_store.codePoint);
+
+    _IsCategories.add(cat2);
+
+    var cat3 = CategoryModel().rebuild((b) => b
+      ..id = 0
+      ..title = "Food"
+      ..desc = "Food related expenses"
+      ..iconCodePoint = Icons.fastfood.codePoint);
+
+    _IsCategories.add(cat3);
+
+    var cat4 = CategoryModel().rebuild((b) => b
+      ..id = 0
+      ..title = "Auto"
+      ..desc = "Car/Bike expences"
+      ..iconCodePoint = Icons.directions_bike.codePoint);
+
+    _IsCategories.add(cat4);
+  }
+
+  Widget _getCategoryTab() {
+    return ListView.builder(
+      itemCount: _IsCategories.length,
+      itemBuilder: (BuildContext ctxt, int index) {
+        var category = _IsCategories[index];
+        return Container(
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(4.0),
+              border: new Border.all(
+                  width: 1.0, style: BorderStyle.solid, color: Colors.white)),
+          margin: EdgeInsets.all(12.0),
+          child: ListTile(
+            onTap: () {},
+            leading: Icon(
+              IconData(category.iconCodePoint, fontFamily: 'MaterialIcons'),
+              color: Theme.of(context).accentColor,
+            ),
+            title: Text(
+              category.title,
+              style: Theme.of(context).textTheme.bodyText2,
+              //.copyWith(color: Theme.of(context).accentColor),
+            ),
+            subtitle: Text(category.desc),
+          ),
+        );
+      },
+    );
   }
 
   @override
